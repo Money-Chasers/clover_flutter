577ce7b633c7c245d7b7ca3c6f7287c9d27bb1f6
Tarun-108 <tarunstu125@gmail.com>
2022-02-02 13:11:52 +0530

    carousel

--- lib/screens/main_application/dashboard_screen/sections/home_screen.dart
+++ lib/screens/main_application/dashboard_screen/sections/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,65 +12,70 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
+  List<String> images = [
+    "https://firebasestorage.googleapis.com/v0/b/clover-650d4.appspot.com/o/carousel_imgs%2FC0.png?alt=media&token=bc76ea05-5a45-40d5-a476-3141efe92b1d",
+"https://firebasestorage.googleapis.com/v0/b/clover-650d4.appspot.com/o/carousel_imgs%2FC1.jfif?alt=media&token=76dc3b36-e8f6-4168-a1cc-e63037f7e019",
+    ""
+  ];
 
-  _buildCards(String text) {
-    return (Container(
-      padding: const EdgeInsets.all(5),
-      child: GestureDetector(
-        onTap: (){
-          Navigator.of(context).pushNamed("paper-display",arguments: [text].toList());
-        },
-        child: Container(
-          width: 150,
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          decoration: BoxDecoration(
-              color: const Color(0xffd4d4d4),
-              borderRadius: BorderRadius.circular(4),
-              border: Border.all(width: 1)),
-          child: Center(
-            child: Text(
-              text,
-              style:
-              GoogleFonts.prompt(textStyle: const TextStyle(fontSize: 20)),
-            ),
-          ),
-        ),
-      ),
-    ));
-  }
+  int activePage = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(10),
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+    return Column(
+      children: [
+        Stack(
           children: [
-            Text(
-              "Try these!",
-              style: GoogleFonts.oswald(
-                textStyle:
-                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-            ),
-            const SizedBox(height: 10),
-            SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
+            carouselPlugin(images),
+            Positioned(
+              top: 180,
+              left: 150,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  _buildCards('Chemistry'),
-                  _buildCards('Physics'),
-                  _buildCards('Maths'),
-                ],
+                children: indicators(images.length, activePage),
               ),
-            ),
+            )
           ],
         ),
-      ),
+      ],
     );
   }
+
+  carouselPlugin(images) {
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 200.0,
+        enlargeCenterPage: true,
+        onPageChanged: (position, reason) {
+          setState(() {
+            activePage = position;
+          });
+        },
+        enableInfiniteScroll: true,
+      ),
+      items: images.map<Widget>((i) {
+        return Builder(
+          builder: (BuildContext context) {
+            return Container(
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: NetworkImage(i))));
+          },
+        );
+      }).toList(),
+    );
+  }
+
+  List<Widget> indicators(imagesLength, currentIndex) {
+    return List<Widget>.generate(imagesLength, (index) {
+      return Container(
+        margin: const EdgeInsets.all(3),
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            color: currentIndex == index ? Colors.black : Colors.black26,
+            shape: BoxShape.circle),
+      );
+    });
+  }
 }
-
-
